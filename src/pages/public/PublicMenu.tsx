e9dc966933d0538df9e5bc668f2b360a623212bf
digitalfenixpro <admin@digitalfenixpro.com>
2025-10-17 19:42:35 -0500

    Updated PublicMenu.tsx

--- src/pages/public/PublicMenu.tsx
+++ src/pages/public/PublicMenu.tsx
@@ -201,7 +201,7 @@ export const PublicMenu: React.FC = () => {
       */}
 
       {/* HEADER */}
-      <header className="sticky top-0 z-50 relative backdrop-blur-md bg-white/30 border-b border-white/20">
+      <header className="sticky top-0 z-50 relative backdrop-blur-md bg-white/30 border-b border-white/10">
         <div className="max-w-7xl mx-auto px-4 py-4">
           <div className="flex items-center justify-between gap-4">
             {/* Search Bar */}
