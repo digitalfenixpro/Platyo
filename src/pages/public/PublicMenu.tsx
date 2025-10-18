f8add420ffa41e5b09b8b35d2aac8311ed3e1d69
digitalfenixpro <admin@digitalfenixpro.com>
2025-10-17 20:05:34 -0500

    Updated PublicMenu.tsx

--- src/pages/public/PublicMenu.tsx
+++ src/pages/public/PublicMenu.tsx
@@ -338,7 +338,7 @@ export const PublicMenu: React.FC = () => {
                       }}
                       onClick={() => isCenter && setSelectedProduct(product)}
                     >
-                      <div className="flex flex flex-col items-center rounded-lg bg-white rounded-lg shadow-xl px-10 py-4">
+                      <div className="  flex flex-col items-center rounded-lg bg-white rounded-lg shadow-xl px-10 py-4">
                         <img
                           src={product.images[0]}
                           alt={product.name}
