98e387d805dc04dda0a63010dd924ec4c2b27500
digitalfenixpro <admin@digitalfenixpro.com>
2025-10-17 19:59:48 -0500

    Updated PublicMenu.tsx

--- src/pages/public/PublicMenu.tsx
+++ src/pages/public/PublicMenu.tsx
@@ -342,7 +342,7 @@ export const PublicMenu: React.FC = () => {
                         <img
                           src={product.images[0]}
                           alt={product.name}
-                          className="w-80 h-80 object-cover  shadow-2xl"
+                          className="w-80 h-80 object-cover rounded-lg  shadow-2xl"
                         />
                         {isCenter && (
                           <div className="mt-6 bg-white rounded-lg shadow-xl px-8 py-4 max-w-xs">
