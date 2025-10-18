e96f53a87676992406df06acac596b9f0a642851
digitalfenixpro <admin@digitalfenixpro.com>
2025-10-17 20:46:17 -0500

    Updated PublicMenu.tsx

--- src/pages/public/PublicMenu.tsx
+++ src/pages/public/PublicMenu.tsx
@@ -396,7 +396,7 @@ export const PublicMenu: React.FC = () => {
                   <ChevronRight className="w-6 h-6" style={{ color: textColor }} />
                 </button>
 
-                <div className="absolute -bottom-8 left-0 right-0 flex justify-center gap-5">
+                <div className="absolute -bottom-8 left-0 top-10 right-0 flex justify-center gap-2">
                   {featuredProducts.map((_, index) => (
                     <button
                       key={index}
