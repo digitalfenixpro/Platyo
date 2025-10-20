79ce594853b2f934eedb5421832dd526580af7b1
digitalfenixpro <admin@digitalfenixpro.com>
2025-10-20 18:32:10 -0500

    Updated PublicMenu.tsx

--- src/pages/public/PublicMenu.tsx
+++ src/pages/public/PublicMenu.tsx
@@ -8,8 +8,8 @@ import { ProductDetail } from '../../components/public/ProductDetail';
 import { CartSidebar } from '../../components/public/CartSidebar';
 import { CheckoutModal } from '../../components/public/CheckoutModal';
 import LeftBlob from './LeftBlob';
-import LeftShape from '../../components/svg/LeftShape';
-import RightShape from '../../components/svg/RightShape';
+import {LeftShape} from '../../components/svg/LeftShape';
+import {RightShape} from '../../components/svg/RightShape';
 
 export const PublicMenu: React.FC = () => {
   const { slug } = useParams<{ slug: string }>();
