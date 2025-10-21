eb7a20404f2c290db63803d351f04dbca7232b3a
digitalfenixpro <admin@digitalfenixpro.com>
2025-10-20 21:53:26 -0500

    Updated PublicMenu.tsx

--- src/pages/public/PublicMenu.tsx
+++ src/pages/public/PublicMenu.tsx
@@ -7,8 +7,6 @@ import { useCart } from '../../contexts/CartContext';
 import { ProductDetail } from '../../components/public/ProductDetail';
 import { CartSidebar } from '../../components/public/CartSidebar';
 import { CheckoutModal } from '../../components/public/CheckoutModal';
-import { useState } from 'react';
-import { Search, Star, ChevronLeft, ChevronRight } from 'lucide-react';
 
 
 export const PublicMenu: React.FC = () => {
