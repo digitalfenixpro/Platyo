a848d866f04986cd976aef54792aa15fd8c0589f
digitalfenixpro <admin@digitalfenixpro.com>
2025-10-21 16:21:40 -0500

    Updated PublicMenu.tsx

--- src/pages/public/PublicMenu.tsx
+++ src/pages/public/PublicMenu.tsx
@@ -546,7 +546,7 @@ export const PublicMenu: React.FC = () => {
 
       {/* CATEGORIES TABS - CENTERED */}
       <div className="relative z-20" >
-        <div className="max-w-7xl mx-auto px-2 py-4 relative">
+        <div className="max-w-6xl mx-auto px-2 py-4 relative">
           <div className="flex gap-3 overflow-x-auto scrollbar-hide justify-center">
             <button
               onClick={() => setSelectedCategory('all')}
