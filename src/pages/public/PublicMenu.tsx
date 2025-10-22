f544404d082bdf0b0fd494b61f1f59f7b0b88357
digitalfenixpro <admin@digitalfenixpro.com>
2025-10-22 16:26:04 -0500

    Updated PublicMenu.tsx

--- src/pages/public/PublicMenu.tsx
+++ src/pages/public/PublicMenu.tsx
@@ -195,7 +195,12 @@ export const PublicMenu: React.FC = () => {
           opacity-80 
           pointer-events-none 
           w-[400px] h-[400px]
-      ">
+          /* Versión móvil */
+          bottom-0 left-1/2 -translate-x-1/2 translate-y-0
+      
+          /* Versión escritorio */
+          sm:top-20 sm:right-0 sm:bottom-auto sm:left-auto sm:translate-x-[25%] sm:-translate-y-[25%]
+        ">
       </Pathbottom>
         
       <Pathtop color={primaryColor} className="
