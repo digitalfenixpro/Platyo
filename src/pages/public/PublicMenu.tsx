7ac2917d295967cde00fe0630b7d20ce856047fb
digitalfenixpro <admin@digitalfenixpro.com>
2025-10-21 20:29:52 -0500

    Updated PublicMenu.tsx

--- src/pages/public/PublicMenu.tsx
+++ src/pages/public/PublicMenu.tsx
@@ -7,7 +7,7 @@ import { useCart } from '../../contexts/CartContext';
 import { ProductDetail } from '../../components/public/ProductDetail';
 import { CartSidebar } from '../../components/public/CartSidebar';
 import { CheckoutModal } from '../../components/public/CheckoutModal';
-import Shapebottom from '/home/project/src/components/public/Shapebottom.tsx'
+import Shapebottom from '../../components/public/Shapebottom'
 
 
 
