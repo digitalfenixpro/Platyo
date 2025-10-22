22c959424748f7b5cfbaa0682212269c45920f95
digitalfenixpro <admin@digitalfenixpro.com>
2025-10-21 21:30:43 -0500

    Updated PublicMenu.tsx

--- src/pages/public/PublicMenu.tsx
+++ src/pages/public/PublicMenu.tsx
@@ -314,8 +314,9 @@ export const PublicMenu: React.FC = () => {
               {/* DF:OPEN/CLOSED STATUS BUTTON */}
               <button
                 onClick={() => setShowHoursModal(true)}
-                className="flex items-center gap-2 px-3 py-3 rounded-lg transition-all hover:opacity-90 shadow-lg"
+                className="flex items-center gap-2 px-3 py-3 rounded-lg transition-all hover:opacity-90 shadow-lg "
                 style={{
+                  borderColor: cardBackgroundColor,
                   backgroundColor: (() => {
                     const now = new Date();
                     const dayNames = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday'];
