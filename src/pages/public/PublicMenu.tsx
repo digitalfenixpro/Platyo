373daeb674483401ceb7e68e2ed01629a6617a6d
digitalfenixpro <admin@digitalfenixpro.com>
2025-10-21 20:44:17 -0500

    Updated PublicMenu.tsx

--- src/pages/public/PublicMenu.tsx
+++ src/pages/public/PublicMenu.tsx
@@ -153,6 +153,8 @@ export const PublicMenu: React.FC = () => {
   const hasPromo = restaurant.settings.promo?.enabled && restaurant.settings.promo?.vertical_promo_image;
 
   return (
+    <Shapebottom/>
+    
     <div
       className="min-h-screen relative overflow-hidden"
       style={{
