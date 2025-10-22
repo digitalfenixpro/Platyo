4a82d21370848cac688a95f8d86b3b80dcfdb879
digitalfenixpro <admin@digitalfenixpro.com>
2025-10-21 21:00:39 -0500

    Updated PublicMenu.tsx

--- src/pages/public/PublicMenu.tsx
+++ src/pages/public/PublicMenu.tsx
@@ -7,7 +7,7 @@ import { useCart } from '../../contexts/CartContext';
 import { ProductDetail } from '../../components/public/ProductDetail';
 import { CartSidebar } from '../../components/public/CartSidebar';
 import { CheckoutModal } from '../../components/public/CheckoutModal';
-import Pathbottom from '../../components/public/Vector (1).svg';
+import Pathbottom from '../../components/public/Pathformbottom.svg';
 
 
 
