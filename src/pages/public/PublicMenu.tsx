2710142cb8c83a686f65ccb7da317a07da246f21
digitalfenixpro <admin@digitalfenixpro.com>
2025-10-21 20:10:59 -0500

    Updated PublicMenu.tsx

--- src/pages/public/PublicMenu.tsx
+++ src/pages/public/PublicMenu.tsx
@@ -8,7 +8,7 @@ import { ProductDetail } from '../../components/public/ProductDetail';
 import { CartSidebar } from '../../components/public/CartSidebar';
 import { CheckoutModal } from '../../components/public/CheckoutModal';
  
-import {Shapesvgbottom} from 'src/components/svg/Shapesvgbottom.tsx';
+import {Shapesvgbottom} from '../../components/svg/Shapesvgbottom.tsx';
 
 
 export const PublicMenu: React.FC = () => {
