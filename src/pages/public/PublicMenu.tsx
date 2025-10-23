3273865c25488f3b1bf089521606097a346b90ae
digitalfenixpro <admin@digitalfenixpro.com>
2025-10-22 21:08:26 -0500

    Updated PublicMenu.tsx

--- src/pages/public/PublicMenu.tsx
+++ src/pages/public/PublicMenu.tsx
@@ -430,7 +430,7 @@ export const PublicMenu: React.FC = () => {
             >
               destacados
             </h2>
-            <div className="flex items-left justify-center gap-1">
+            <div className="flex items-left justify-left gap-1">
               {[1,2,3,4,5].map(i => (
                 <Star key={i} className="w-5 h-5 fill-current" style={{ color: secondaryColor }} />
               ))}
