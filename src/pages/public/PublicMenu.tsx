abdbb5454fa2c7319425258fb81c979c3e770376
digitalfenixpro <admin@digitalfenixpro.com>
2025-10-22 20:36:08 -0500

    Updated PublicMenu.tsx

--- src/pages/public/PublicMenu.tsx
+++ src/pages/public/PublicMenu.tsx
@@ -181,7 +181,7 @@ export const PublicMenu: React.FC = () => {
       <Pathleft color={primaryColor} className="
           absolute
           top-10 -left-1 
-          w-[141px] h-[431px]
+          w-[253px] h-auto
           pointer-events-none
         "> 
       </Pathleft>
