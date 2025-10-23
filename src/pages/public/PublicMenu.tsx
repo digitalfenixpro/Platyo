c41e799a1c1b2decb1b711eb1a11699345e6f328
digitalfenixpro <admin@digitalfenixpro.com>
2025-10-22 21:15:28 -0500

    Updated PublicMenu.tsx

--- src/pages/public/PublicMenu.tsx
+++ src/pages/public/PublicMenu.tsx
@@ -183,7 +183,7 @@ export const PublicMenu: React.FC = () => {
           -top-20
           left-0
           opacity-80
-          w-[340px] 
+          w-[240px] 
           h-[800px]
           -translate-y-[10%]
           pointer-events-none
