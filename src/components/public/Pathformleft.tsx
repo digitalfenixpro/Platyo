5d8ad76022c3320df40fb52480691f7de56dfd60
digitalfenixpro <admin@digitalfenixpro.com>
2025-10-22 20:13:37 -0500

    Updated Pathformleft.tsx

--- src/components/public/Pathformleft.tsx
+++ src/components/public/Pathformleft.tsx
@@ -1,6 +1,6 @@
 import * as React from "react"
 
-function Pathformtop(props) {
+function Pathformleft(props) {
   const { color } = props;
   return(
     <svg width="253" height="272" viewBox="0 0 253 272" fill={color} xmlns="http://www.w3.org/2000/svg"{...props}>
@@ -11,4 +11,4 @@ function Pathformtop(props) {
   
 }
 
-export default Pathformtop;
+export default Pathformleft;
