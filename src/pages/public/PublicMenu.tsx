950f6f298ed71b178450c480e1cdd5e803e21fde
digitalfenixpro <admin@digitalfenixpro.com>
2025-10-22 20:55:35 -0500

    Updated PublicMenu.tsx

--- src/pages/public/PublicMenu.tsx
+++ src/pages/public/PublicMenu.tsx
@@ -180,7 +180,7 @@ export const PublicMenu: React.FC = () => {
       
       <Pathleft color={primaryColor} className="
           absolute
-          top-1
+          top-0
           left-0
           opacity-80
           w-[350px] 
