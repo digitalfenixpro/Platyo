9dc2aa21ef152800e939e6b5f7f7f492d16d2bb1
digitalfenixpro <admin@digitalfenixpro.com>
2025-10-22 21:14:42 -0500

    Updated PublicMenu.tsx

--- src/pages/public/PublicMenu.tsx
+++ src/pages/public/PublicMenu.tsx
@@ -613,8 +613,8 @@ export const PublicMenu: React.FC = () => {
           </div>
         ) : (
           <div className={
-            viewMode === 'list' ? 'grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2' :
-            viewMode === 'grid' ? 'grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2' :
+            viewMode === 'list' ? 'grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4' :
+            viewMode === 'grid' ? 'grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4' :
             'space-y-6'
           }>
             {filteredProducts.map((product) => {
